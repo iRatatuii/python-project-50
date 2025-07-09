b405c7cce15adf3b173123745c0c660a793af02f
iratatui <andreikarpenkin123@gmail.com>
2025-07-09 16:06:03 +0300

    test: fix ruff

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,14 +1,15 @@
-from gendiff.scripts.gendiff import parse_file, generate_diff
 import os
+
+from gendiff.scripts.gendiff import generate_diff, parse_file
 
 
 def test_generate_diff():
-    path1 = os.path.join('tests', 'fixtures', 'file1')
-    path2 = os.path.join('tests', 'fixtures', 'file2')
-    
-    data1 = parse_file(path1, 'json')
-    data2 = parse_file(path2, 'json')
-    
+    path1 = os.path.join("tests", "fixtures", "file1")
+    path2 = os.path.join("tests", "fixtures", "file2")
+
+    data1 = parse_file(path1, "json")
+    data2 = parse_file(path2, "json")
+
     result = generate_diff(data1, data2)
     # file1 = {
     # "host": "hexlet.io",
@@ -22,15 +23,14 @@ def test_generate_diff():
     # "verbose": True,
     # "host": "hexlet.io"
     # }
-    
-    expected = '''{
+
+    expected = """{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
   + timeout: 20
   + verbose: true
-}'''
+}"""
 
     assert result == expected
-    
