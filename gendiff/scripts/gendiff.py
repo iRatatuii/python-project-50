f4281ed10ff7821778239c27188cffbaa301e1b2
iratatui <andreikarpenkin123@gmail.com>
2025-07-09 13:07:12 +0300

    style: fix spaces for ruff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,74 +3,52 @@ import json
 
 
 def parse_file(path, format):
-    path = f'{path}.{format}'
-    with open(path, 'r') as file:
+    path = f"{path}.{format}"
+    with open(path, "r") as file:
         return json.load(file)
-
-
-# def generate_diff(file1, file2):
-#     all_item = file1 | file2
-#     result = ''
-#     for key, value in sorted(all_item.items()):
-#         if file1.get(key, None) == file2.get(key):
-#             result += f'    {key}: {str(value).lower()}\n'
-#         elif file1.get(key, None) is None:
-#             result += f'  + {key}: {str(value).lower()}\n'
-#         elif file2.get(key, None) is None:
-#             result += f'  - {key}: {str(value).lower()}\n'
-#         else:
-#             result += f'  - {key}: {str(file1[key]).lower()}\n'
-#             result += f'  + {key}: {str(file2[key]).lower()}\n'
     
-#     print(f'''{{
-# {result}}}''')
+    
 def generate_diff(file1, file2):
     all_item = file1 | file2
     result = []
-    
+
     for key in sorted(all_item):
         value1 = file1.get(key)
         value2 = file2.get(key)
-        
+
         if value1 == value2:
-            result.append(f'    {key}: {str(value1).lower()}')
+            result.append(f"    {key}: {str(value1).lower()}")
         elif key not in file1:
-            result.append(f'  + {key}: {str(value2).lower()}')
+            result.append(f"  + {key}: {str(value2).lower()}")
         elif key not in file2:
-            result.append(f'  - {key}: {str(value1).lower()}')
+            result.append(f"  - {key}: {str(value1).lower()}")
         else:
-            result.append(f'  - {key}: {str(file1[key]).lower()}')
-            result.append(f'  + {key}: {str(file2[key]).lower()}')
-    diff_output = '{\n' + '\n'.join(result) + '\n}'
+            result.append(f"  - {key}: {str(file1[key]).lower()}")
+            result.append(f"  + {key}: {str(file2[key]).lower()}")
+    diff_output = "{\n" + "\n".join(result) + "\n}"
     return diff_output
-    
-    
+
+
 def main():
     parser = argparse.ArgumentParser(
-        prog='gendiff',
-        usage='%(prog)s [-h] [-f FORMAT] first_file second_file',
-        description='Compares two configuration files and shows a difference.'
+        prog="gendiff",
+        usage="%(prog)s [-h] [-f FORMAT] first_file second_file",
+        description="Compares two configuration files and shows a difference.",
     )
-    
-    parser.add_argument('first_file', nargs='+')
-    parser.add_argument('second_file', nargs='+')
-    
-    parser.add_argument('-f', '--format', nargs='?', 
-                        help='set format of output')
+
+    parser.add_argument("first_file", nargs="+")
+    parser.add_argument("second_file", nargs="+")
+
+    parser.add_argument(
+        "-f", "--format", nargs="?", help="set format of output"
+    )
     args = parser.parse_args()
-    # print(args)
+    
     file1 = args.first_file[0]
     file2 = args.second_file[0]
-    format = args.format
 
-    data1 = parse_file(file1, format)
-    data2 = parse_file(file2, format)
-    
-    # print(data1)
-    # print(data2)
-    
+    data1 = parse_file(file1, args.format)
+    data2 = parse_file(file2, args.format)
+
     result = generate_diff(data1, data2)
     print(result)
-    # if args == '-h' or args == '--help':
-    #     parser.print_help()
-    
