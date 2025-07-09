8185be5c0388d2c5efcaacc2df2c5b662cd2ec2e
iratatui <andreikarpenkin123@gmail.com>
2025-07-09 13:07:46 +0300

    test: add test for generate_diff

---
+++ tests/test_diff.py
@@ -0,0 +1,29 @@
+from gendiff.scripts.gendiff import generate_diff
+import json
+import pytest
+
+
+def test_generate_diff():
+    file1 = {
+        "host": "localhost",
+        "timeout": 50,
+        "verbose": True
+    }
+
+    file2 = {
+        "host": "localhost",
+        "timeout": 20,
+        "debug": True
+    }
+
+    expected = '''{
+  + debug: true
+    host: localhost
+  - timeout: 50
+  + timeout: 20
+  - verbose: true
+}'''
+
+    result = generate_diff(file1, file2)
+    assert result == expected
+    
