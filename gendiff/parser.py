2ca3b377306533a994e54aff4094ddd968eee9ef
iratatui <andreikarpenkin123@gmail.com>
2025-07-11 13:31:02 +0300

    fix: rename variable format_

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -4,9 +4,9 @@ import yaml
 
 
 def parse_file(path):
-    _, format = path.split('.')
+    _, format_ = path.split('.')
     
-    if format in ["yml", "yaml"]:
+    if format_ in ["yml", "yaml"]:
         with open(path, "r") as yaml_file:
             yaml_object = yaml.safe_load(yaml_file)
             return yaml_object
