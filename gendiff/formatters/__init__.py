6dd4ba6a4290fd75ee0437592ed6c90cdb3444e0
iratatui <andreikarpenkin123@gmail.com>
2025-07-11 12:23:55 +0300

    feat: add new json formatter style

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,5 +1,6 @@
-from gendiff.formatters.plain import format_plain
-from gendiff.formatters.stylish import format_stylish
+from .json import format_json
+from .plain import format_plain
+from .stylish import format_stylish
 
 
 def get_formatter(format_name):
@@ -8,5 +9,7 @@ def get_formatter(format_name):
             return format_stylish
         case 'plain':
             return format_plain
+        case 'json':
+            return format_json
         case _:
             raise ValueError(f"Unsupported format: {format_name}")
